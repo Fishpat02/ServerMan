f5d99228916d58207957e233fa697a78f1e7e583
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2022-07-05 00:07:04 -0400

    Updated user.ts to have an option

--- src/commands/user.ts
+++ src/commands/user.ts
@@ -1,10 +1,17 @@
-import { ApplicationCommandTypes } from 'discord.js/typings/enums'
+import { ApplicationCommandOptionTypes, ApplicationCommandTypes } from 'discord.js/typings/enums'
 import { CommandModule } from '../templates/commandModule'
 
 export const User: CommandModule = {
   name: 'user',
   description: 'Replies with the user\'s information!',
   type: ApplicationCommandTypes.CHAT_INPUT,
+  options: [
+    {
+      name: 'user',
+      description: 'The user to pick.',
+      type: ApplicationCommandOptionTypes.USER,
+    },
+  ],
 
   async run(client, interaction) {
     const content = `User tag: ${interaction.user.tag}\nUser id: ${interaction.user.id}`
