f9d00bf1ac28e1992c8fc7138f21ee4ac20adc55
Rachel Fallis <55009589+Fishpat02@users.noreply.github.com>
2023-01-01 21:58:38 -0500

    Updated deplay-commands.ts

--- src/scripts/deploy-commands.ts
+++ src/scripts/deploy-commands.ts
@@ -8,3 +8,7 @@ const rest = new REST({ version:'10' }).setToken(process.env.TOKEN!)
 rest.put(Routes.applicationGuildCommands(process.env.CLIENT!, process.env.GUILD!), { body: Commands })
   .then(() => console.log(`Successfully registered ${Commands.length} application commands.`))
   .catch(console.error)
+
+rest.put(Routes.applicationCommands(process.env.CLIENT!),
+  { body: Commands },
+)
