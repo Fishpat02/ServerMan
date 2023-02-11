7baa9c091c3ea906df779ffa38c6cc04b7febadc
= <=>
2023-02-11 03:48:31 -0500

    Added new delete-channel command

--- src/commands.ts
+++ src/commands.ts
@@ -1,8 +1,10 @@
 import { CreateChannel } from './commands/create-channel'
 import { CreateThread } from './commands/create-thread'
+import { DeleteChannel } from './commands/delete-channel'
 import { CommandModule } from './templates/commandModule'
 
 export const Commands: CommandModule[] = [
   CreateChannel,
   CreateThread,
+  DeleteChannel,
 ]
