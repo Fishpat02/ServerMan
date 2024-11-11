edee840681ae3ebb8df050d097a920ed48d4c5e4
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2024-11-11 15:34:58 -0500

    Updated scripts to use Deno environment instead of Node

--- src/scripts/deploy-commands.ts
+++ src/scripts/deploy-commands.ts
@@ -1,45 +1,47 @@
-import 'dotenv/config'
 import { Commands } from '../commands.ts'
-import { REST } from '@discordjs/rest'
-import { Routes } from 'discord-api-types/v10'
-import process from "node:process";
+import { REST, Routes } from 'discord.js'
 
-const args = process.argv.slice(2)
+const args = Deno.args[0]
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN!)
+const rest = new REST({ version: '10' }).setToken(Deno.env.get('TOKEN')!)
 
 const registerGuildCommands = () => {
   rest
     .put(
-      Routes.applicationGuildCommands(process.env.CLIENT!, process.env.GUILD!),
+      Routes.applicationGuildCommands(
+        Deno.env.get('CLIENT')!,
+        Deno.env.get('GUILD')!,
+      ),
       { body: Commands },
     )
     .then(() =>
       console.log(
         `Successfully registered ${Commands.length} guild commands.`,
-      ),
+      )
     )
     .catch(console.error)
 }
 
 const registerGlobalCommands = () => {
   rest
-    .put(Routes.applicationCommands(process.env.CLIENT!), { body: Commands })
+    .put(Routes.applicationCommands(Deno.env.get('CLIENT')!), {
+      body: Commands,
+    })
     .then(() => {
       console.log(`Successfully registered ${Commands.length} global commands`)
     })
     .catch(console.error)
 }
 
-switch (args.pop()) {
-case 'guild':
-  registerGuildCommands()
-  break
-case 'global':
-  registerGlobalCommands()
-  break
-default:
-  registerGuildCommands()
-  registerGlobalCommands()
-  break
+switch (args) {
+  case 'guild':
+    registerGuildCommands()
+    break
+  case 'global':
+    registerGlobalCommands()
+    break
+  default:
+    registerGuildCommands()
+    registerGlobalCommands()
+    break
 }
