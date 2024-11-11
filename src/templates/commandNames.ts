baf11671a0f42ff4e1b7615669da467706abd0c6
Fishpat02 <55009589+Fishpat02@users.noreply.github.com>
2024-11-11 11:38:45 -0500

    Added enums for command option names

---
+++ src/templates/commandNames.ts
@@ -0,0 +1,19 @@
+export const enum BaseCommandNames {
+  CreateChannel = 'create-channel',
+  CreateThread = 'create-thread',
+  DeleteChannel = 'delete-channel',
+}
+
+export const enum CommandNames {
+  NewCategory = 'new-category',
+  NoCategory = 'no-category',
+  WithCategory = 'with-category',
+}
+
+export const enum SubCommandNames {
+  Category = 'category',
+  CategoryName = 'category-name',
+  Channel = 'channel',
+  ChannelName = 'channel-name',
+  ThreadName = 'thread-name',
+}
